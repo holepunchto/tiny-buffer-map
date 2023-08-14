c4b8d5c93ba89b18d1f5321ee1a552fbadadb900
genesisblockhead <110317592+genesisblockhead@users.noreply.github.com>
2023-08-14 17:36:35 -0400

    Actually use binary encoding.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const b4a = require('b4a')
 
-const enc = 'binary'
+const encoding = 'binary'
 
 module.exports = class BufferMap {
   constructor (other) {
@@ -12,34 +12,34 @@ module.exports = class BufferMap {
   }
 
   get (key) {
-    if (b4a.isBuffer(key)) key = b4a.toString(key, 'hex')
+    if (b4a.isBuffer(key)) key = b4a.toString(key, encoding)
     return this.m.get(key)
   }
 
   set (key, value) {
-    if (b4a.isBuffer(key)) key = b4a.toString(key, 'hex')
+    if (b4a.isBuffer(key)) key = b4a.toString(key, encoding)
     return this.m.set(key, value)
   }
 
   delete (key) {
-    if (b4a.isBuffer(key)) key = b4a.toString(key, 'hex')
+    if (b4a.isBuffer(key)) key = b4a.toString(key, encoding)
     return this.m.delete(key)
   }
 
   has (key) {
-    if (b4a.isBuffer(key)) key = b4a.toString(key, 'hex')
+    if (b4a.isBuffer(key)) key = b4a.toString(key, encoding)
     return this.m.has(key)
   }
 
   * [Symbol.iterator] () {
     for (const [key, value] of this.m) {
-      yield [b4a.from(key, 'hex'), value]
+      yield [b4a.from(key, encoding), value]
     }
   }
 
   * keys () {
     for (const key of this.m.keys()) {
-      yield b4a.from(key, 'hex')
+      yield b4a.from(key, encoding)
     }
   }
 
